b53724785173ccc8dac8dd3f474ffb3371b10721
IrhamFR <irham.f.rufdo@gmail.com>
2022-09-26 12:32:07 +0700

    update

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func main() {
 	var AllowedOrigins = handlers.AllowedOrigins([]string{"*"})
 
 	// var port = "5000"
-	var port = os.Getenv("5432")
+	var port = os.Getenv("PORT")
 	fmt.Println("server running localhost:" + port)
 
 	http.ListenAndServe("localhost:"+port, handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
